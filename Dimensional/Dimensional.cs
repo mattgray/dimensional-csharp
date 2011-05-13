b368e0f6fcc40b4485ed9150d6624396879ba7f2
Matt Gray <matthew.thomas.gray@gmail.com>
2011-05-13 17:55:53 +0100

    subtraction, a bit of commenting and cleanup

--- Dimensional/Dimensional.cs
+++ Dimensional/Dimensional.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Dimensional
+﻿namespace Dimensional
 {
+    //dimension base type
     public abstract class Dimension
     {   
     }
+
+    //A couple of dimensions to be getting started with
+    
+    public class Length : Dimension
+    {
+    }
+
+    public class Time : Dimension
+    {
+    }
+
+    //product and quotient
 
     public class Product<TDimension1, TDimension2> : Dimension
         where TDimension1 : Dimension
@@ -21,13 +29,7 @@ namespace Dimensional
     {
     }
 
-    public class Length : Dimension
-    {
-    }
-
-    public class Time : Dimension
-    {
-    }
+    //A given quantity of a certain dimension
 
     public class Quantity<TDimension>  where TDimension : Dimension
     {
@@ -41,6 +43,11 @@ namespace Dimensional
         public Quantity<TDimension> Add(Quantity<TDimension> toAdd)
         {
             return new Quantity<TDimension>(_amount + toAdd._amount);
+        }
+
+        public Quantity<TDimension> Subtract(Quantity<TDimension> toSubtract)
+        {
+            return new Quantity<TDimension>(_amount - toSubtract._amount);
         }
 
         public Quantity<Product<TDimension, TDimension2>> MultipliedBy<TDimension2>(Quantity<TDimension2> toMultiplyBy) 
@@ -61,11 +68,11 @@ namespace Dimensional
         public void Blah()
         {
             //Test: Quantity construction
-            var tenUnitLengths = new Quantity<Length>(10f);
+            var tenUnitLengths = new Quantity<Length>(10);
 
-            var fiveUnitLengths = new Quantity<Length>(5f);
+            var fiveUnitLengths = new Quantity<Length>(5);
 
-            var twoUnitsOfTime = new Quantity<Time>(2f);
+            var twoUnitsOfTime = new Quantity<Time>(2);
 
             //Test: Addition
             var fifteenLengthUnits = tenUnitLengths.Add(fiveUnitLengths);
@@ -73,6 +80,10 @@ namespace Dimensional
             var fourUnitsOfTime = twoUnitsOfTime.Add(twoUnitsOfTime);
 
             //var cantAddLengthToTime = tenUnitLengths.Add(twoUnitsOfTime);
+
+            //Test: Subtraction
+
+            var threeUnitsOfTime = fourUnitsOfTime.Subtract(new Quantity<Time>(1));
 
             //Test: Multiplication
             var hundredLengthUnitsSquared = tenUnitLengths.MultipliedBy(tenUnitLengths);
@@ -85,15 +96,14 @@ namespace Dimensional
 
             var fiveLengthPerTimeUnits = tenUnitLengths.DivideBy(twoUnitsOfTime);
 
-            var twentyLengthUnitsDodgy = fiveLengthPerTimeUnits.MultipliedBy(new Quantity<Time>(4f));
-
-
-            //Now, it would be nice if these were to compile, thus removing the Product / Quotient constructors:
+            var twentyLengthUnitsDodgy = fiveLengthPerTimeUnits.MultipliedBy(new Quantity<Time>(4));
+                      
+            //Now, it would be nice if these were to compile, removing the nested Product / Quotient constructors:
 
             /*
             Quantity<Length> twentyLengthUnitsNice = hundredLengthUnitsSquared.DivideBy(fiveUnitLengths);
 
-            Quantity<Length> twentyLengthUnitsAlsoNice = fiveLengthPerTimeUnits.MultipliedBy(new Quantity<Time>(4f));
+            Quantity<Length> twentyLengthUnitsAlsoNice = fiveLengthPerTimeUnits.MultipliedBy(new Quantity<Time>(4));
             */
         }
     }
